a5775694e60cb753f2d4eef213eb07db406bc513
jorge <jym272@pop-os.localdomain>
2022-04-23 04:09:56 -0300

    Se optimiza imagenes para produccion

--- next.config.js
+++ next.config.js
@@ -1,23 +1,35 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    //https://moviesplace.shop
-    //http://localhost:8080
-    env: {
-        APP_API: 'https://moviesplace.shop',
-    },
-    images: {
-        domains: ["image.tmdb.org"],
-    },
-    reactStrictMode: true,
-    async redirects() {
-        return [
-            {
-                source: '/home',
-                destination: '/',
-                permanent: true,
-            },
-        ]
-    },
-}
+const {PHASE_DEVELOPMENT_SERVER} = require('next/constants')
 
-module.exports = nextConfig
+
+module.exports = (phase, {defaultConfig}) => {
+    const nextConfig = {
+        env: {
+            APP_API: 'https://api.moviesplace.shop',
+        },
+        images: {
+            domains: ["image.tmdb.org"],
+        },
+        reactStrictMode: true,
+        async redirects() {
+            return [
+                {
+                    source: '/home',
+                    destination: '/',
+                    permanent: true,
+                },
+            ]
+        },
+    }
+
+    if (phase === PHASE_DEVELOPMENT_SERVER) {
+        return {
+            ...nextConfig,
+            env: {
+                APP_API: 'http://localhost:8080',
+            },
+        }
+    }
+    /* config options for all phases except development here */
+    return nextConfig
+}
