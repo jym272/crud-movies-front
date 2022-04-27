d1a42a3a97b83b7472cd5a9f7b914100e7aebb6d
jorge <jym272@pop-os.localdomain>
2022-04-27 04:27:14 -0300

    v1.0.0

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -10,11 +10,13 @@ export const AboutComponent = () => {
             <h1>About</h1>
             <p>This is a CRUD app v1.0.0. Written by Jorge Clavijo. Email: jym272@gmail.com.</p>
             <div>Frontend: React ( NextJS Framework )</div>
-            <div>
             <a target="_blank" rel="noopener noreferrer" href="https://github.com/jym272/crud-movies-front">Repository</a>
-                </div>
-            <p>Backend: Written in Go, implements a REST and GraphQL API&apos;s</p>
-            <div>Database: PostgreSQL</div>
+            <p>Database: PostgreSQL</p>
+
+            <div>Backend: Written in Go, implements a REST and GraphQL API&apos;s</div>
+            <a target="_blank" rel="noopener noreferrer" href="https://github.com/jym272/crud-movies-back">Repository</a>
+
+
             <p>Web Server: Caddy &nbsp; / &nbsp;Server: Google Cloud VM instance</p>
 
         </div>
